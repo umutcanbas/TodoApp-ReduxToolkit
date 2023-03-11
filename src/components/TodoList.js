a156cb1b2bdb89024852a875f813da07a17457fc
Umutcan BAŞ <umutbasinfo@gmail.com>
2023-03-12 02:40:23 +0300

    Delete button activeted

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,44 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle, destroy } from "../redux/todos/todosSlice";
+let filtered = [];
 
 const TodoList = () => {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
+
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
+
+  filtered = items;
+  if (activeFilter !== "all") {
+    filtered = items.filter((todo) =>
+      activeFilter === "active"
+        ? todo.completed === false && todo
+        : todo.completed === true && todo
+    );
+  }
 
   return (
     <ul className="todo-list">
-      {items.map((item) => (
+      {filtered.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title} </label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
