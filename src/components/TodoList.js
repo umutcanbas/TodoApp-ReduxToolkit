297eac8e931ae3b19efddea9afc7411723bb6ddc
Umutcan BAŞ <umutbasinfo@gmail.com>
2023-03-12 01:44:08 +0300

    Delete expamle data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-
 const TodoList = () => {
-  const items= useSelector(state=>state.todos.items)
-  
+  const items = useSelector((state) => state.todos.items);
+
   return (
     <ul className="todo-list">
-    {/*   <li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-      </li> */}
-      
-     {
-      items.map((item)=>(
-        <li key={item.id} >
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>{item.title} </label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-
-      ))
-     }
+      {items.map((item) => (
+        <li key={item.id} className={item.completed ? "completed" : ""}>
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            <label>{item.title} </label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
