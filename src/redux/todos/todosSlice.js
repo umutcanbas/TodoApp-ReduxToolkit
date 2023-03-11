d55f4bb3c2ad4457ca9f2eadb951599a14231070
Umutcan BAŞ <umutbasinfo@gmail.com>
2023-03-12 01:44:31 +0300

    Updata todosSlice

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -7,14 +7,22 @@ export const todosSlice = createSlice({
       {
         id: "1",
         title: "Learn React",
+        completed: true,
       },
       {
         id: "2",
         title: "Read a book",
+        completed: false,
       },
     ],
   },
-  reducers: {},
+  reducers: {
+    addTodo:(state , action)=>{
+      state.items.push(action.payload)
+    }
+  },
 });
 
+
+export const {addTodo} = todosSlice.actions;
 export default todosSlice.reducer;
